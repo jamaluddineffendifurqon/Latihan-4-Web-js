ae1aaf4e9ba66cb1832c5a21341a832019069695
jamaluddineffendifurqon <jamaluddineffendifurqon@gmail.com>
2024-04-25 21:21:55 +0700

    first commit

---
+++ isi.js
@@ -0,0 +1,5 @@
+document.writeln("<CENTER>");
+document.writeln("<H1>PT KERETA EXPRESS PADANG</H1>");
+document.writeln("<H3>Silahkan Isi Form Dibawah Untuk Memesan Tiket<H3>");
+document.writeln("<HR WIDTH=600 COLOR=Black>");
+document.writeln("</CENTER>");
